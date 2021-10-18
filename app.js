a3c71d0e767539dded0a56daf03433a223fb2ace
jessie-yook <sprout.jessie@gmail.com>
2021-10-18 23:09:40 +0900

    🚧  Study booleans (#2.4)

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 2.2 variables
-const a = 5
-const b = 2
-const myName = 'Jessie'
+const a = 5;
+const b = 2;
+const myName = 'Jessie';
 
 console.log(a + b);
 console.log(a * b);
@@ -16,12 +16,23 @@ console.log('This is ' + myName);
 // let 재선언 금지, 재할당 가능 >> sometimes
 // var 재선언 가능, 재할당 가능 >> never use
 
-let anotherName = 'Jessie'
+let anotherName = 'Jessie';
 console.log('This is ' + myName);
 
-anotherName = 'Subin' // also you dont have to use 'let' repeatedly
+anotherName = 'Subin'; // also you dont have to use 'let' repeatedly
 
 console.log('Now, this is ' + myName);
 
 
 
+// 2.4 booleans
+// true = 1
+// flase = 0
+// null = 값이 없음. 비어있음 (We use null to make sure something that is nothing.)
+// undefined = 값이 정의되지 않음. 컴퓨터가 인지하고 있지만 값이 없음
+
+const amIFat = null; //의도적으로 값을 넣지 않음 = null
+let areYouFat; //값을 정의하지 않음 = undefined (+ const로는 값이 없는 variable이 안만들어진다)
+
+console.log(amIFat);
+console.log(areYouFat);
