456034098ab28ad3eda9bbefea2b0d9fc85086be
jessie-yook <sprout.jessie@gmail.com>
2021-10-18 22:49:10 +0900

    🚧  Study const and let (+var) (#2.3)

--- app.js
+++ app.js
@@ -8,3 +8,20 @@ console.log(a * b);
 console.log(a / b);
 
 console.log('This is ' + myName);
+
+
+
+// 2.3 const and let (+var)
+// const 재선언 금지, 재할당 금지 >> always
+// let 재선언 금지, 재할당 가능 >> sometimes
+// var 재선언 가능, 재할당 가능 >> never use
+
+let anotherName = 'Jessie'
+console.log('This is ' + myName);
+
+anotherName = 'Subin' // also you dont have to use 'let' repeatedly
+
+console.log('Now, this is ' + myName);
+
+
+
