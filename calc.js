d3eb5c66932fba3c2044f4eaa532548afe726dda
jessie-yook <sprout.jessie@gmail.com>
2021-10-23 17:01:42 +0900

    🚧  Made calculate object with Functions

---
+++ calc.js
@@ -0,0 +1,24 @@
+const clac = {
+    add: function (a, b) {
+        console.log(a + b);
+    },
+
+    minus: function (a, b) {
+        console.log(a - b);
+    },
+
+    divide: function (a, b) {
+        console.log(a / b);
+    },
+
+    power: function (a, b) {
+        console.log(a ** b);
+    },
+}
+
+clac.add(5, 10);
+clac.minus(25, 5);
+clac.divide(30, 4);
+clac.power(2, 3);
+
+
