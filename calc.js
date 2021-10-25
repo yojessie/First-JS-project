46e2e7b1333c1c23dfe80789e33e55ea6872ab67
jessie-yook <sprout.jessie@gmail.com>
2021-10-25 22:14:42 +0900

    🚧  Study Returns (#2.11)

--- calc.js
+++ calc.js
@@ -1,24 +1,54 @@
+// const clac = {
+//     plus: function (a, b) {
+//         console.log(a + b);
+//     },
+
+//     minus: function (a, b) {
+//         console.log(a - b);
+//     },
+
+//     divide: function (a, b) {
+//         console.log(a / b);
+//     },
+
+//     power: function (a, b) {
+//         console.log(a ** b);
+//     },
+// }
+
+// clac.add(5, 10);
+// clac.minus(25, 5);
+// clac.divide(30, 4);
+// clac.power(2, 3);
+
+
+// Return (#2.11)
 const clac = {
-    add: function (a, b) {
-        console.log(a + b);
+    plus: function (a, b) {
+        console.log('Do you wanna build a snow man?');
+        return a + b;
+        console.log('OKbye'); // return 뒤의 코드는 실행되지 않는다
     },
 
-    minus: function (a, b) {
-        console.log(a - b);
+    minus: function(a, b) {
+        return a - b;
     },
 
-    divide: function (a, b) {
-        console.log(a / b);
+    divide: function(a, b) {
+        return a / b
     },
 
     power: function (a, b) {
-        console.log(a ** b);
+        return a ** b;
     },
 }
 
-clac.add(5, 10);
-clac.minus(25, 5);
-clac.divide(30, 4);
-clac.power(2, 3);
+const plusResult = clac.plus(2, 3);
+const minusResult = clac.minus(plusResult, 5);
+const divideResult = clac.divide(10, plusResult);
+const powerResult = clac.power(divideResult, plusResult);
 
-
+console.log(plusResult);
+console.log(minusResult);
+console.log(divideResult);
+console.log(powerResult);
