a218032e70c0fd8c4a10e053aa067eb94b61283f
jessie-yook <sprout.jessie@gmail.com>
2021-10-25 23:15:26 +0900

    🚧  Study conditionals (#2.13 ~ 2.15)

--- calc.js
+++ calc.js
@@ -52,3 +52,30 @@ console.log(plusResult);
 console.log(minusResult);
 console.log(divideResult);
 console.log(powerResult);
+
+
+
+
+
+// Conditionals (#2.13)
+// const ageTest = prompt('How old are you?');
+
+// console.log(typeof ageTest); // variable의 type을 알 수 있는 코드
+// console.log(parseInt(ageTest)); // 기본값인 string을 number로 바꿔주는 코드
+
+const age = parseInt(prompt('How old are you?')); // Function은 안쪽 코드부터 바깥쪽으로 실행된다
+
+console.log(age);
+console.log(isNaN(age)); // Number의 여부를 boolean으로 보여주는 코드
+
+if (isNaN(age) || age < 0) {
+    console.log('Please write a real positive number');
+} else if (age < 18) {
+    console.log('You are too young to drink');
+} else if (age >= 18 && age <= 50) {
+    console.log('Enjoy your drink');
+    // 조건 둘 다 충족하게 만들때 && 을 사용한다 (= And operator = And 연산자)
+    // Or operaotr는 || 를 사용
+} else {
+    console.log('I am worrying about your condition');
+}
